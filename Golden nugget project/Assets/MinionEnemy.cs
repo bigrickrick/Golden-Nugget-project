242ae4f41b45cf622825172cd7ab424fe1b5e77f
bigrickrick <122563862+bigrickrick@users.noreply.github.com>
2023-11-22 23:39:28 -0500

    updated the ai of enemies

--- Golden nugget project/Assets/MinionEnemy.cs
+++ Golden nugget project/Assets/MinionEnemy.cs
@@ -4,28 +4,25 @@ using UnityEngine;
 
 public class MinionEnemy : EnemyScript
 {
-    public override void Move()
+    
+    public override void ChaseTarget()
     {
-        if (Player.Instance != null)
-        {
+        Vector3 direction = Target.position - transform.position;
+        direction.Normalize();
 
 
-            Vector3 directionToPlayer = Player.Instance.PlayerPosition.position - transform.position;
-
-
-            directionToPlayer.Normalize();
-
-
-            transform.Translate(directionToPlayer * gameObject.GetComponent<Entity>().EntitySpeed * Time.deltaTime);
-            EnemyLookAtPlayer();
-        }
+        transform.Translate(direction * gameObject.GetComponent<Entity>().EntitySpeed * Time.deltaTime);
+        EnemyLookAtTarget();
     }
-    public override void EnemyLookAtPlayer()
+    public override void EnemyLookAtTarget()
     {
-        transform.LookAt(Player.Instance.PlayerPosition.position);
+        transform.LookAt(Target.position);
     }
-    public override void EnemyShoot()
+    public override void EnemyAttack()
     {
+        
+        EnemyLookAtTarget();
+
         GameObject bul = Instantiate(bullet, firePoint.position, Quaternion.identity);
 
 
@@ -36,35 +33,9 @@ public class MinionEnemy : EnemyScript
 
         rb.velocity = shootingDirection * bulletSpeed;
     }
-    public override bool PlayerInshootrange()
-    {
-        Vector3 directionToPlayer = Player.Instance.PlayerPosition.position - transform.position;
-        float angleToPlayer = Vector3.Angle(transform.forward, directionToPlayer);
-        float distanceToPlayer = Vector3.Distance(transform.position, Player.Instance.PlayerPosition.position);
+    
 
-
-        return angleToPlayer <= detectionAngle * 0.5f && distanceToPlayer <= detectionRange;
-    }
-
-    private void Update()
-    {
-
-        Move();
-        
-        if (PlayerInshootrange())
-        {
-
-            ShootingTimer -= Time.deltaTime;
-            if (ShootingTimer <= 0)
-            {
-                EnemyShoot();
-                ShootingTimer = baseShootingTimer / gameObject.GetComponent<Entity>().attackspeedModifier;
-            }
-            
-        }
-
-
-    }
+    
 
 
 }
