cc2d35aaa349e93b496689dd2ecd948e322ca187
bigrickrick <122563862+bigrickrick@users.noreply.github.com>
2023-12-05 12:54:36 -0500

    finish emergancy map

--- Golden nugget project/Assets/PassiveManager.cs
+++ Golden nugget project/Assets/PassiveManager.cs
@@ -18,157 +18,161 @@ public class PassiveManager : MonoBehaviour
     private void Update()
     {
         Debug.Log("has died " + Player.Instance.EnemyHasdied());
-        foreach(PassiveAugments passive in passiveAugmentslist)
+        if(passiveAugmentslist.Count > 0)
         {
-            PassiveAugments.PassiveType type = passive.GetPassiveType();
-            if (type == PassiveAugments.PassiveType.OnKill)
+            foreach (PassiveAugments passive in passiveAugmentslist)
             {
-                switch (state)
+                PassiveAugments.PassiveType type = passive.GetPassiveType();
+                if (type == PassiveAugments.PassiveType.OnKill)
                 {
-                    case PassiveState.ready:
-                        DurationTime = passive.Duration;
-                        if (Player.Instance.EnemyHasdied()== true)
-                        {
-                            passive.ActivatePassive();
-                            state = PassiveState.active;
-                        }
-                        break;
-                    case PassiveState.active:
-                        
-                        if (DurationTime > 0)
-                        {
-                            Debug.Log("Buff is activated");
-                            DurationTime -= Time.deltaTime;
-                        }
-                        else
-                        {
-                            state = PassiveState.cooldown;
-                        }
-                        break;
-                    case PassiveState.cooldown:
-                        passive.SetToOriginalState();
-                        Player.Instance.HasEnemyHasdied(false);
-                        state = PassiveState.ready;
-                        break;
-                }
-            }
-            else if (type == PassiveAugments.PassiveType.OnHit)
-            {
-                switch (state)
-                {
-                    case PassiveState.ready:
-                        DurationTime = passive.Duration;
-                        if (Player.Instance.hasHitSomething == true)
-                        {
-                            passive.ActivatePassive();
-                            state = PassiveState.active;
-                        }
-                        break;
-                    case PassiveState.active:
+                    switch (state)
+                    {
+                        case PassiveState.ready:
+                            DurationTime = passive.Duration;
+                            if (Player.Instance.EnemyHasdied() == true)
+                            {
+                                passive.ActivatePassive();
+                                state = PassiveState.active;
+                            }
+                            break;
+                        case PassiveState.active:
 
-                        if (DurationTime > 0)
-                        {
-                            Debug.Log("Buff is activated");
-                            DurationTime -= Time.deltaTime;
-                            
-                        }
-                        else
-                        {
-                            state = PassiveState.cooldown;
-                            
-                        }
-                        break;
-                    case PassiveState.cooldown:
-                        passive.SetToOriginalState();
-                        Player.Instance.hasHitSomething = false;
-                        state = PassiveState.ready;
-                        break;
-                }
-            }
-            else if(type == PassiveAugments.PassiveType.AlwaysActive)
-            {
-                switch (state)
-                {
-                    case PassiveState.ready:
-                        DurationTime = passive.Duration;
-                        cooldowntime = passive.CooldownPassive;
-                        
-                        state = PassiveState.active;
-                        break;
-                    case PassiveState.active:
-
-                        if (DurationTime > 0)
-                        {
-                            Debug.Log("Buff is activated");
-                            passive.ActivatePassive();
-                            DurationTime -= Time.deltaTime;
-
-                        }
-                        else
-                        {
-                            state = PassiveState.cooldown;
-
-                        }
-                        break;
-                    case PassiveState.cooldown:
-                        passive.SetToOriginalState();
-                        if(cooldowntime > 0)
-                        {
-                            cooldowntime -= Time.deltaTime;
-                        }
-                        else
-                        {
+                            if (DurationTime > 0)
+                            {
+                                Debug.Log("Buff is activated");
+                                DurationTime -= Time.deltaTime;
+                            }
+                            else
+                            {
+                                state = PassiveState.cooldown;
+                            }
+                            break;
+                        case PassiveState.cooldown:
+                            passive.SetToOriginalState();
+                            Player.Instance.HasEnemyHasdied(false);
                             state = PassiveState.ready;
-                        }
-                        
-                        break;
+                            break;
+                    }
                 }
-            }
-            else if (type == PassiveAugments.PassiveType.OnAbilityActivate)
-            {
-                switch (state)
+                else if (type == PassiveAugments.PassiveType.OnHit)
                 {
-                    case PassiveState.ready:
-                        DurationTime = passive.Duration;
-                        cooldowntime = passive.CooldownPassive;
-                        if(Player.Instance.movementAbility.abilityActivated== true || Player.Instance.utilityAbilityHolder.abilityActivated == true)
-                        {
-                            state = PassiveState.active;
-                        }
+                    switch (state)
+                    {
+                        case PassiveState.ready:
+                            DurationTime = passive.Duration;
+                            if (Player.Instance.hasHitSomething == true)
+                            {
+                                passive.ActivatePassive();
+                                state = PassiveState.active;
+                            }
+                            break;
+                        case PassiveState.active:
 
-                        
-                        break;
-                    case PassiveState.active:
+                            if (DurationTime > 0)
+                            {
+                                Debug.Log("Buff is activated");
+                                DurationTime -= Time.deltaTime;
 
-                        if (DurationTime > 0)
-                        {
-                            Debug.Log("Buff is activated");
-                            passive.ActivatePassive();
-                            DurationTime -= Time.deltaTime;
+                            }
+                            else
+                            {
+                                state = PassiveState.cooldown;
 
-                        }
-                        else
-                        {
-                            state = PassiveState.cooldown;
-
-                        }
-                        break;
-                    case PassiveState.cooldown:
-                        passive.SetToOriginalState();
-                        if (cooldowntime > 0)
-                        {
-                            cooldowntime -= Time.deltaTime;
-                        }
-                        else
-                        {
+                            }
+                            break;
+                        case PassiveState.cooldown:
+                            passive.SetToOriginalState();
+                            Player.Instance.hasHitSomething = false;
                             state = PassiveState.ready;
-                        }
-
-                        break;
+                            break;
+                    }
                 }
+                else if (type == PassiveAugments.PassiveType.AlwaysActive)
+                {
+                    switch (state)
+                    {
+                        case PassiveState.ready:
+                            DurationTime = passive.Duration;
+                            cooldowntime = passive.CooldownPassive;
+
+                            state = PassiveState.active;
+                            break;
+                        case PassiveState.active:
+
+                            if (DurationTime > 0)
+                            {
+                                Debug.Log("Buff is activated");
+                                passive.ActivatePassive();
+                                DurationTime -= Time.deltaTime;
+
+                            }
+                            else
+                            {
+                                state = PassiveState.cooldown;
+
+                            }
+                            break;
+                        case PassiveState.cooldown:
+                            passive.SetToOriginalState();
+                            if (cooldowntime > 0)
+                            {
+                                cooldowntime -= Time.deltaTime;
+                            }
+                            else
+                            {
+                                state = PassiveState.ready;
+                            }
+
+                            break;
+                    }
+                }
+                else if (type == PassiveAugments.PassiveType.OnAbilityActivate)
+                {
+                    switch (state)
+                    {
+                        case PassiveState.ready:
+                            DurationTime = passive.Duration;
+                            cooldowntime = passive.CooldownPassive;
+                            if (Player.Instance.movementAbility.abilityActivated == true || Player.Instance.utilityAbilityHolder.abilityActivated == true)
+                            {
+                                state = PassiveState.active;
+                            }
+
+
+                            break;
+                        case PassiveState.active:
+
+                            if (DurationTime > 0)
+                            {
+                                Debug.Log("Buff is activated");
+                                passive.ActivatePassive();
+                                DurationTime -= Time.deltaTime;
+
+                            }
+                            else
+                            {
+                                state = PassiveState.cooldown;
+
+                            }
+                            break;
+                        case PassiveState.cooldown:
+                            passive.SetToOriginalState();
+                            if (cooldowntime > 0)
+                            {
+                                cooldowntime -= Time.deltaTime;
+                            }
+                            else
+                            {
+                                state = PassiveState.ready;
+                            }
+
+                            break;
+                    }
+                }
+
+
             }
-
-
         }
+        
     }
 }
