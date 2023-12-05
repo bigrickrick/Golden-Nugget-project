227ea836b083ca38859fa9c86ba285c3e9075d47
bigrickrick <122563862+bigrickrick@users.noreply.github.com>
2023-12-04 19:18:07 -0500

    Boss stuff

---
+++ Golden nugget project/Assets/BossScript.cs
@@ -0,0 +1,255 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BossScript : MonoBehaviour
+{
+    public int damage;
+    protected Transform Target;
+
+    public string targetstring;
+
+    public LayerMask whatisground, whatisplayer;
+
+    public Vector3 walkPoint;
+    private bool walkPointSet;
+    public float walkPointRange;
+    public bool AlreadyAttacked;
+    public float Attackrange;
+    
+    public float detectionRange;
+    public float BaseTimeBeforeTeleportingAttack;
+    public float BaseTimeBeforeSummonAllies;
+    private float TimeBeforeTeleportingAttack;
+    private float TimeBeforeSummonAllies;
+
+    public bool TargetInRangedAttackRange, TeleportingAttackReady, SummonAlliesReady, TargetInSightRange;
+
+    public enum EnemyState
+    {
+        Patroling,
+        ChaseTarget,
+        DashAttack,
+        RangedAttack,
+        TeleportingAttack,
+        SummonAllies,
+
+    }
+    public EnemyState enemyState = EnemyState.ChaseTarget;
+    
+    public GameObject bullet;
+    public Transform firePoint;
+
+    public float bulletSpeed = 35f;
+    public float baseShootingTimer;
+
+    
+    [SerializeField] protected Transform[] BulletSpawnPoint;
+    private float ShootingTimer;
+
+    private void Start()
+    {
+        SetTarget(targetstring);
+        SetShootingTimer();
+    }
+    public void SetShootingTimer()
+    {
+        ShootingTimer = baseShootingTimer / gameObject.GetComponent<Entity>().attackspeedModifier;
+    }
+
+
+    public void SetTarget(string target)
+    {
+        Target = GameObject.Find(target).transform;
+    }
+
+    public void Patroling()
+    {
+        if (!walkPointSet)
+        {
+            SearchWalkPoint();
+        }
+
+        if (walkPointSet)
+        {
+            GoToWalkPoint();
+        }
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        if (distanceToWalkPoint.magnitude < 1f)
+        {
+            walkPointSet = false;
+        }
+    }
+    private void GoToWalkPoint()
+    {
+        Vector3 direction = walkPoint - transform.position;
+        direction.Normalize();
+
+
+        transform.Translate(direction * gameObject.GetComponent<Entity>().EntitySpeed * Time.deltaTime);
+    }
+    public abstract void ChaseTarget();
+    public abstract void EnemyAttack();
+    public abstract void EnemyLookAtTarget();
+
+    public void SearchWalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatisground))
+        {
+            walkPointSet = true;
+        }
+    }
+    public virtual void DashAttack() { }
+    public virtual void TeleportingAttack() { }
+    public virtual void SummonAllies() { }
+
+    public float dashCooldown = 5.0f; 
+    private float dashTimer = 0.0f;
+    public float BaseDashDuration;
+    protected float DashDuration = 0;
+    public bool IsDashing;
+    private void Update()
+    {
+        
+        TargetInRangedAttackRange = Physics.CheckSphere(transform.position, Attackrange, whatisplayer);
+        TargetInSightRange = Physics.CheckSphere(transform.position, detectionRange, whatisplayer);
+        TimeBeforeTeleportingAttack -= Time.deltaTime;
+        TimeBeforeSummonAllies -= Time.deltaTime;
+        Debug.Log(TimeBeforeTeleportingAttack);
+        switch (enemyState)
+        {
+            case EnemyState.Patroling:
+                if (TargetInSightRange)
+                {
+                    enemyState = EnemyState.ChaseTarget;
+                }
+                else
+                {
+                    Patroling();
+                }
+                break;
+
+            case EnemyState.ChaseTarget:
+                if (TimeBeforeTeleportingAttack <= 0)
+                {
+                    TeleportingAttackReady = true;
+                    enemyState = EnemyState.TeleportingAttack;
+                }
+
+                if (TimeBeforeSummonAllies <= 0)
+                {
+                    SummonAlliesReady = true;
+                }
+
+                if (!TargetInSightRange)
+                {
+                    enemyState = EnemyState.Patroling;
+                    break;
+                }
+                else if (TargetInRangedAttackRange)
+                {
+                    enemyState = EnemyState.RangedAttack;
+                }
+                else if (TeleportingAttackReady)
+                {
+                    enemyState = EnemyState.TeleportingAttack;
+                }
+                else if (SummonAlliesReady)
+                {
+                    SummonAlliesReady = false;
+                    enemyState = EnemyState.SummonAllies;
+                }
+                else
+                {
+                    ChaseTarget();
+                }
+                break;
+
+            case EnemyState.RangedAttack:
+                if (!TargetInRangedAttackRange)
+                {
+                    enemyState = EnemyState.ChaseTarget;
+                }
+                else
+                {
+                    if (ShootingTimer <= 0)
+                    {
+                        EnemyAttack();
+                        AlreadyAttacked = true;
+                        SetShootingTimer();
+                    }
+                    else
+                    {
+                        ChaseTarget();
+                        ShootingTimer -= Time.deltaTime;
+                    }
+                }
+                break;
+
+            case EnemyState.TeleportingAttack:
+                if (TeleportingAttackReady)
+                {
+                    TeleportingAttack();
+                    TeleportingAttackReady = false;
+                    TimeBeforeTeleportingAttack = BaseTimeBeforeTeleportingAttack;
+                }
+                else
+                {
+                    enemyState = EnemyState.ChaseTarget;
+                }
+                break;
+
+            case EnemyState.SummonAllies:
+                if (SummonAlliesReady)
+                {
+                    SummonAllies();
+                    SummonAlliesReady = false;
+                    TimeBeforeSummonAllies = BaseTimeBeforeSummonAllies;
+                }
+                else
+                {
+                    enemyState = EnemyState.ChaseTarget;
+                }
+                break;
+
+        }
+        if (dashTimer > 0)
+        {
+            dashTimer -= Time.deltaTime;
+        }
+        else
+        {
+            if (!IsDashing)
+            {
+                DashAttack();
+            }
+            DashDuration -= Time.deltaTime;
+            if(DashDuration <= 0)
+            {
+                gameObject.GetComponent<Entity>().EntitySpeed = 10;
+                IsDashing = false;
+                dashTimer = dashCooldown;
+                DashDuration = BaseDashDuration;
+            }
+            
+        }
+        
+    }
+    public bool HasDied = false;
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireSphere(transform.position, Attackrange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, detectionRange);
+       
+        
+    }
+}
---
+++ Golden nugget project/Assets/DarkOne.cs
@@ -0,0 +1,129 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarkOne : BossScript
+{
+    public override void ChaseTarget()
+    {
+        Vector3 direction = Target.position - transform.position;
+        direction.Normalize();
+
+
+        transform.Translate(direction * gameObject.GetComponent<Entity>().EntitySpeed * Time.deltaTime);
+        EnemyLookAtTarget();
+    }
+    public ParticleSystem dashParticles; 
+    public ParticleSystem teleportParticles;
+    public override void EnemyAttack()
+    {
+        EnemyLookAtTarget();
+
+        GameObject bul = Instantiate(bullet, firePoint.position, Quaternion.identity);
+        bul.GetComponent<Enemybullet>().SetBulletDamage(damage);
+
+        Vector3 shootingDirection = (Target.position - firePoint.position).normalized;
+
+        Rigidbody rb = bul.GetComponent<Rigidbody>();
+
+
+        rb.velocity = shootingDirection * bulletSpeed;
+    }
+    public override void EnemyLookAtTarget()
+    {
+        transform.LookAt(Target);
+    }
+    public int dashSpeed;
+
+    public override void DashAttack()
+    {
+        gameObject.GetComponent<Entity>().EntitySpeed += dashSpeed;
+        IsDashing = true;
+
+        if (dashParticles != null)
+        {
+            var particleInstance = Instantiate(dashParticles, transform.position, Quaternion.identity);
+            particleInstance.transform.parent = transform;
+            particleInstance.Play();
+        }
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.CompareTag(targetstring))
+        {
+            if (IsDashing)
+            {
+                Entity player = collision.gameObject.GetComponent<Entity>();
+                if (player != null)
+                {
+                    player.DamageRecieve(damage);
+                    
+                }
+            }
+        }
+        
+    }
+    public int BaseTeleportationstack;
+    private int teleportationstack;
+    public override void TeleportingAttack()
+    {
+        StartCoroutine(TeleportWithDelay());
+    }
+
+    IEnumerator TeleportWithDelay()
+    {
+        float teleportRange = 15f;
+
+        for (int i = 0; i < BaseTeleportationstack; i++)
+        {
+            if (teleportationstack != BaseTeleportationstack)
+            {
+                float randomX = Random.Range(Target.position.x - teleportRange, Target.position.x + teleportRange);
+                float randomZ = Random.Range(Target.position.z - teleportRange, Target.position.z + teleportRange);
+                Vector3 randomPosition = new Vector3(randomX, transform.position.y, randomZ);
+
+                if (teleportParticles != null)
+                {
+                    var particleInstance = Instantiate(teleportParticles, transform.position, Quaternion.identity);
+                    particleInstance.transform.parent = transform;
+                    particleInstance.Play();
+                    Destroy(particleInstance.gameObject, particleInstance.main.duration);
+                }
+
+                yield return new WaitForSeconds(0.5f);
+
+                transform.position = randomPosition;
+
+                EnemyAttack();
+            }
+        }
+    }
+    public GameObject allyPrefab;
+    public int maxAlliesToSummon = 3;
+    public float maxSummonRadius = 10f;
+
+    public override void SummonAllies()
+    {
+        for (int i = 0; i < maxAlliesToSummon; i++)
+        {
+            Vector3 spawnPosition = GetRandomSpawnPosition(); 
+            Quaternion spawnRotation = Quaternion.identity; 
+
+            Instantiate(allyPrefab, spawnPosition, spawnRotation);
+        }
+    }
+
+    Vector3 GetRandomSpawnPosition()
+    {
+        
+        Vector2 randomCircle = Random.insideUnitCircle.normalized * maxSummonRadius;
+
+        Vector3 bossPosition = transform.position;
+        Vector3 spawnPosition = bossPosition + new Vector3(randomCircle.x, 0f, randomCircle.y);
+
+        return spawnPosition;
+    }
+
+
+}
